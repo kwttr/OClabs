d9bb5aa03added2efc183f50f1b3411592a215aa
unknown <kerbixcs@mail.ru>
2022-05-24 13:44:56 +0700

    to fix: convert object to int[]

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -2,39 +2,75 @@
 
 class program
 {
-    public void FindAverage()
+
+    static void FindAverage(object argument)
     {
-        Interlocked.Exchange(ref average, i);//ref average = ссылка на double average (по идее), i - число (элемент массива) которое будет прибавлено к average. Как это заставить работать?
+        int[] arr = (int[])argument;
+        int average = 0;
+        foreach (int r in arr)
+        {
+            Console.WriteLine($"i - {r}");
+            average = average + r;
+            Thread.Sleep(12);
+        }
+        average = average / arr.Length;
+        Console.WriteLine("среднее значение:" + average);
     }
-    public static void Main()
+
+    static void Main()
     {
+        //объявление переменных
+        int max = 0,min = 0;
+        bool key = true;
+        int[] arr=null;
+        double average = 0;
+        ref double avgRef = ref average;
+        //Ввод размера массива
         Console.WriteLine("Введите размер массива");
-        string num = Console.ReadLine();
-        int[] arr = new int[Int32.Parse(num)];
+        while (key == true)
+        {
+            try
+            {
+                string num = Console.ReadLine();
+                arr = new int[Int32.Parse(num)];
+                key = false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неправильный ввод, введите еще раз");
+                key = true;
+            }
+        }
 
         //a)Заполнение массива случайными числами
         Random rand = new Random();
         foreach (int i in arr)
         {
             arr[i] = rand.Next(0, 10000000);
+            Console.WriteLine(arr[i]);
         }
-
+        ref int[] arrRef = ref arr;
         //b)Создание потока worker
-        double average = 0;
-        Thread worker = new Thread();        //не знаю как подписаться на функцию. пойду спать.
-        worker.Start();
+        ParameterizedThreadStart workerThread = new ParameterizedThreadStart(FindAverage);
+        Thread worker = new Thread(workerThread);
+        worker.Start(arrRef);
 
         //c)	Найти минимальный и максимальный элементы массива и вывести
         //их на консоль.После каждого сравнения элементов «спать» 7
         //миллисекунд(для этого использовать функцию Sleep(время_в_миллисекундах)).
-
-        int max = arr[0];
-        int min = arr[0];
+        max = arr[0];
+        min = arr[0];
         foreach (int i in arr)
         {
             if (i > max) max = i;
             if (i < min) min = i;
             Thread.Sleep(7);
         }
+        //d)Дождаться завершения worker
+        //?????
+
+        //e)	Подсчитать количество элементов в массиве, значение которых
+        //больше среднего значения элементов массива, и вывести его на
+
     }
 }
---
+++ testproject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+class program
+{
+    static void Main(string[] args)
+    {
+        var c = 0;
+        var thread = new Thread(() =>
+        {
+            c = Count();
+        });
+        thread.Start();
+        thread.Join();
+        Console.WriteLine(c.ToString());
+        Console.ReadLine();
+    }
+
+    public int Count()
+    {
+        int A = 1;
+        int B = 2;
+        return A + B;
+    }
+}
